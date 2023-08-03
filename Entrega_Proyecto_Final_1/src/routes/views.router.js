a10beeff66eeb161fd7a405c0b52d500b54928c9
julianbischoff <julibischoff@gmail.com>
2023-08-03 12:13:09 -0600

    problema_view_routes_buscar_id

--- Entrega_Proyecto_Final_1/src/routes/views.router.js
+++ Entrega_Proyecto_Final_1/src/routes/views.router.js
@@ -4,22 +4,33 @@ const viewsRouter = express.Router();
 
 
 
-// viewsRouter.get('/productos', async (request, response) => {
-//   const products = await productsModel.find().lean().exec();
-//   response.render('home', { products });
-// });
-
-
 viewsRouter.get('/productos', async (request, response) => {
   const products = await productsModel.find().lean().exec();
   response.render('home', { products });
 });
 
 
+viewsRouter.get('/list', async (request, response) => {
+  const result = await productsModel.paginate({},{
+    page: 1,
+    limit: 10,
+    lean:true,
+  });
+  response.render('productosList', result);
+});
+
+
 viewsRouter.get('/productos/:pid', async (request, response) => {
-  const id = request.params.pid;
-  const product = await productsModel.findById(id);
-  response.render('one', product);
+  try {
+    const id = request.params.pid;
+    const product = await productsModel.findById(id);
+    if (!product) {
+      return response.status(404).send(`no se encuentra el id `);
+    }
+    response.render('one', { product });
+  } catch (error) {
+    response.status(404).send('Error');
+  }
 });
 
 viewsRouter.get('/chat', async (request, response) => {
