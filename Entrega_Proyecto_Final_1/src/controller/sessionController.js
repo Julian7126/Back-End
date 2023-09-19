069fcd9906f135d6319add28ec2f4211b1e85a16
julianbischoff <julibischoff@gmail.com>
2023-09-18 23:38:37 -0300

    comentarios}

--- Entrega_Proyecto_Final_1/src/controller/sessionController.js
+++ Entrega_Proyecto_Final_1/src/controller/sessionController.js
@@ -5,14 +5,13 @@ import config from "../config/config.js";
 export const login = async (request, response) => {
   try {
     const { user, access_token } = await sessionService.loginUser(request.user);
-    
+    // console.log("user payload", user.payload)
     console.log("User:", user); // Depuración
     console.log("Access Token:", access_token); // Depuración
     
-    // Guardar el objeto 'user' completo en la sesión
+  //  request.session.user=user.payload
     request.session.user = user;
     
-    // Establecer la cookie con el token de acceso
     response.cookie(config.PRIVATE_KEY_COOKIE, access_token, { maxAge: 24 * 60 * 60 * 1000 });
     
     // Redirigir al usuario
@@ -47,7 +46,7 @@ export const logout = async (request, response) => {
 
 export const currentUser = async (request, response) => {
   try {
-    const user = await sessionService.getCurrentUser(request.user); // Asumiendo que podría ser una operación asíncrona
+    const user = await sessionService.getCurrentUser(request.user);
     return response.status(200).json({ status: 'success', payload: user });
   } catch (error) {
     return response.status(400).json({ status: 'error', message: error.message });
